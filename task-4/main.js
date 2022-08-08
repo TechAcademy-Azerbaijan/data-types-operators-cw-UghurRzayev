7812e228e61eded1293aaa255686730051ebbfd5
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-08 21:30:25 +0400

    Task 4 done

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    var input = result.input;
+    var numbers = input.split(',');
+    var a = parseInt(numbers[0]);
+    var b = parseInt(numbers[1]);
+    var bolme = a/b 
+    console.log(bolme);
 });
